4cfc996dc7d3e0c310c5727d91da32cfda93b44f
Gleb <glebpro1991@gmail.com>
2016-09-16 03:46:29 +0100

    email to lowercase hook added to model

--- models/user.js
+++ models/user.js
@@ -17,5 +17,15 @@ module.exports = function(sequelize, DataTypes) {
                 len: [5, 100]
             }
         }
+    }, {
+        hooks: {
+            beforeValidate: function(user, options) {
+                if(typeof user.email === 'string') {
+                    user.email = user.email.toLowerCase();
+                } else {
+
+                }
+            } 
+        }
     });
 }
